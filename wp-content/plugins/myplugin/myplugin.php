1aa5242a97bb08a44076ecef930fa9d7a2c5e014
Elin Forsberg <elinfsp96@hotmail.com>
2023-04-21 10:53:48 +0200

    add_action remove_action

--- wp-content/plugins/myplugin/myplugin.php
+++ wp-content/plugins/myplugin/myplugin.php
@@ -1,3 +1,15 @@
+
+<?php
 /*
 Plugin Name: myplugin
 */
+
+add_action('the_title', 'my_title');
+
+function my_title($title) {
+$words = str_replace('o', '⚽️', $title);
+return $words;
+}
+
+
+?>
--- wp-content/themes/mychildtheme/functions.php
+++ wp-content/themes/mychildtheme/functions.php
@@ -1,5 +1,7 @@
 <?php
-add_action('after_set_up_theme', 'my_remove_function', 99 );
+
+
+add_action('after_setup_theme', 'my_remove_function', 99 );
 
 function my_remove_function() {
     remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
@@ -11,6 +13,13 @@ add_action('woocommerce_archive_description', 'best_shop');
 
 function best_shop(){
     echo '<h4>Välkommen till världens bästa sportshop!!</h4>';
+}
+
+add_filter('woocommerce_checkout_fields', 'kassa_funktion');
+
+function kassa_funktion($fields){
+    $fields['order']['order_comments']['placeholder'] = 'Valfria beställningsanteckningar...';
+    return $fields;
 }
 
 ?>
